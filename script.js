d04fb6cab2ccffb169bce68f29344bbc6f25dd04
epicbob57 <epicbob5757@gmail.com>
2017-03-19 19:38:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var para = document.createElement("p");
-var node = document.createTextNode("This is new.");
-para.appendChild(node);
+var Discord = require('discord.io');
+var bot = new Discord.Client({
+  autorun: true, 
+  email: "epicbob5757@gmail.com",
+  password: "UBv43DVg$"
+});
+
+bot.on('ready', function() {
+  console.log("Successfully connected: " + bot.username + " - (" + bot.id + ")");
+});
+
+bot.on('message', function(callback) { /* Event called when someone joins the server */
+  console.log('message recieved');
+ });
