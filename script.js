3277d06b204c886a087476532d0aa19f14cf59b2
epicbob57 <epicbob5757@gmail.com>
2017-03-20 15:53:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var Discord = require('discord.io');
 var client = new Discord.Client({
   autorun: true, 
-  token: "";
+  token: ""
 });
  var encrypted = CryptoJS.AES.encrypt("MTk0MjU5OTY5MzU5NDc4Nzg0.C7HttQ.y1fAUHYaJ4BkWNDi1h28hVyZBf4", "UBv43DVg$");
 client.on('ready', function() {
@@ -22,3 +22,4 @@ client.on('message', function(callback) { /* Event called when someone joins the
 
 function serverChange() { 
   
+}
