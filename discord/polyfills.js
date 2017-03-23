499ca460bf607aa082196defee9b3ea27222ff1a
epicbob57 <epicbob5757@gmail.com>
2017-03-22 20:20:05 -0700

    Add files via upload

---
+++ discord/polyfills.js
@@ -0,0 +1,63 @@
+/**
+ * Polyfills, YAY!! (mostly for Windows Phone 8.1)
+ */
+
+// DOM Remove Polyfill (from MDN)
+if (!('remove' in Element.prototype)) {
+  Element.prototype.remove = function () {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  };
+}
+
+// Polyfill for creating CustomEvents on IE9/10/11
+
+// code pulled from:
+// https://github.com/d4tocchini/customevent-polyfill
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
+
+try {
+    var ce = new window.CustomEvent('test');
+    ce.preventDefault();
+    if (ce.defaultPrevented !== true) {
+        // IE has problems with .preventDefault() on custom events
+        // http://stackoverflow.com/questions/23349191
+        throw new Error('Could not prevent default');
+    }
+} catch(e) {
+  var CustomEvent = function(event, params) {
+    var evt, origPrevent;
+    params = params || {
+      bubbles: false,
+      cancelable: false,
+      detail: undefined
+    };
+
+    evt = document.createEvent("CustomEvent");
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+    origPrevent = evt.preventDefault;
+    evt.preventDefault = function () {
+      origPrevent.call(this);
+      try {
+        Object.defineProperty(this, 'defaultPrevented', {
+          get: function () {
+            return true;
+          }
+        });
+      } catch(e) {
+        this.defaultPrevented = true;
+      }
+    };
+    return evt;
+  };
+
+  CustomEvent.prototype = window.Event.prototype;
+  window.CustomEvent = CustomEvent; // expose definition to window
+}
+
+/** Not exactly a polyfill, but rather a hacky way to get some jQuery plugins running on webpack */
+window.jQuery = require('jquery');
+require('../node_modules/semantic-ui-css/semantic.js');
+require('perfect-scrollbar/jquery')(window.jQuery);
+require('./jquery.fullscreen');
