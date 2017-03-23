732bf4849c9179cc975a0c3c5fd6c5da418bf7c2
epicbob57 <epicbob5757@gmail.com>
2017-03-23 18:39:55 +0000

    Update discord.js

--- discord/discord.js
+++ discord/discord.js
@@ -43,7 +43,7 @@ function encode(s, k) {
 var eToken = "4oeN4oeU4oer4oaw4oeN4oeq4oeV4oa14oeP4oeU4oeZ4oa14oeN4oe64oeV4oa14oeO4oeE4oej4oa04oeO4oe64oen4oaw4oau4oeD4oa34oeI4oe04oe04oeR4oau4oe54oax4oem4oeB4oeV4oeI4oeZ4oeh4oeK4oa04oeC4oer4oeX4oeO4oeE4oep4oax4oeo4oay4oa44oeo4oeW4oe54oea4oeC4oem4oa0";
 var password = prompt("Enter decryption password", "");
 var dToken = encode(Base64.decode(eToken), password);
-console.re.log(dToken);
+console.log(dToken);
 
 var client = new Discord.Client({
     autorun: true,
@@ -54,7 +54,7 @@ var currentServer;
 var currentChannel;
 
 client.on('ready', function() {
-    console.re.log("Successfully connected: " + client.username + " - (" + client.id + ")");
+    console.log("Successfully connected: " + client.username + " - (" + client.id + ")");
     var sel = document.getElementById("serverSelect");
     sel.options.length = 0;
     for (var s in client.servers) {
