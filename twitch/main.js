2fe1aa81c39875ebd770606ed3a0669602955882
epicbob57 <epicbob5757@gmail.com>
2017-07-13 16:02:34 -0700

    Update main.js

--- twitch/main.js
+++ twitch/main.js
@@ -12,7 +12,7 @@ var channels = ['grandpoobear'], // Channels to initially join
 
 
 
-
+console.log(window)
 var chat = document.getElementById('chat'),
 	defaultColors = ['rgb(255, 0, 0)','rgb(0, 0, 255)','rgb(0, 128, 0)','rgb(178, 34, 34)','rgb(255, 127, 80)','rgb(154, 205, 50)','rgb(255, 69, 0)','rgb(46, 139, 87)','rgb(218, 165, 32)','rgb(210, 105, 30)','rgb(95, 158, 160)','rgb(30, 144, 255)','rgb(255, 105, 180)','rgb(138, 43, 226)','rgb(0, 255, 127)'],
 	randomColorsChosen = {},
@@ -22,7 +22,7 @@ var chat = document.getElementById('chat'),
 				},
 			channels: channels
 		},
-	client = new tmi.client(clientOptions);
+	client = new window.tmi.client(clientOptions);
 
 function dehash(channel) {
 	return channel.replace(/^#/, '');
